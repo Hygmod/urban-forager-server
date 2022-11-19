5e98b86ff9abeead3b8c1ba261d8914bcc2a7de1
Hygmod <joshsdanton@gmail.com>
2022-11-18 21:00:08 -0800

    updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:3000", "https://urban-forager.onrender.com"]
+const allowedOrigins = ["http://localhost:3000", "https://urban-forager.onrender.com", "100.20.92.101", "44.225.181.72", "44.227.217.144"]
 
 module.exports = allowedOrigins

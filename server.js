6f5d63f8b9741d2915f40486b28b396896e09c1c
Hygmod <joshsdanton@gmail.com>
2022-12-01 19:40:11 -0800

    added auth

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const connectDB = require("./config/dbConn")
 const mongoose = require("mongoose")
 const PORT = process.env.PORT || 3500
 
-console.log("log")
-
 connectDB()
 
 app.use(logger)
@@ -26,6 +24,8 @@ app.use(cookieParser())
 app.use("/", express.static(path.join(__dirname, "public")))
 
 app.use("/", require("./routes/root"))
+app.use("/signup", require("./routes/signupRoutes"))
+app.use("/auth", require("./routes/authRoutes"))
 app.use("/markers", require("./routes/markerRoutes"))
 app.use("/users", require("./routes/userRoutes"))
 app.use("/notes", require("./routes/noteRoutes"))

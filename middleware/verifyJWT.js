952370c4593444aed144a283277228eab9035e9c
Hygmod <joshsdanton@gmail.com>
2022-12-03 21:20:48 -0800

    removed console log

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
     if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
-    console.log(authHeader)
     const token = authHeader.split(' ')[1];
     jwt.verify(
         token,

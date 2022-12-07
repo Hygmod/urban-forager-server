c0e450f7ac4866362f95fadffa0f49ff1d5d4c60
Hygmod <joshsdanton@gmail.com>
2022-12-06 19:25:14 -0800

    updated token auth

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const handleRefreshToken = async (req, res) => {
+    console.log('handleRefreshToken')
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
